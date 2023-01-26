57936197448478ac27600b5e1d9fddd71ba849c3
soloman <shaaheensaab@gmail.com>
2023-01-26 23:51:29 +0600

    array explored

---
+++ array.js
@@ -0,0 +1,17 @@
+// ::::::::::::Array::::::::::::::
+var friendsAge = [23, 13, 10, 48, 39, 105];
+var picnicFee = [5000, 500, 700, 2000, 100];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+console.log(vowels.length);
+
+// get element by index from an Array..
+var roll = [22, 4, 2, 7, 88, 54, 1, 10];
+console.log(roll);
+
+// set element by index
+roll[1] = 5;
+console.log(roll);
+
+// find index of an element
+var positionIndex = roll.indexOf(10);
+console.log(positionIndex);
