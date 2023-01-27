daf7fc8e628f7e03022266a4ef1d919ffcb0ae1b
soloman <shaaheensaab@gmail.com>
2023-01-27 10:40:42 +0600

    multicondition explored

---
+++ multistage-condition.js
@@ -0,0 +1,10 @@
+// ::::::::::MULTI-CONDITION:::::::::::: \\
+var person = "Peter";
+
+if(person == "John"){
+    console.log("Check Again!");
+}else if(person == "Peter"){
+    console.log("Confirmed!");
+}else{
+    console.log("Not okay!");
+}
