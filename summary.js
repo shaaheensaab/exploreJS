a39467124be6dfc4437e0ab77183403caed46034
soloman <shaaheensaab@gmail.com>
2023-01-27 11:00:37 +0600

    summary added

---
+++ summary.js
@@ -0,0 +1,14 @@
+/**
+ * Array is a collection of similar data elements.
+ * Array index indicates the position of the element within the array.
+ * Array length is the total number of the elements in an array.
+ * The indexOf() method return the index (position) of a specified value.
+ * We can replace an element in an array with the help of array index. EX: arrayName[index] = newValue;
+ * The push() method adds new items to the end of an array.
+ * The pop() method removes the last element from the end of an array.
+ * Comparison operators are used to test for true or false.
+ * The if statement executes if the condition is true.
+ * The else statement executes if the condition is false.
+ * Logical operator, &&- returns true if both statements are tre and logical operator, ||- returns the true if one of the statements is true.
+ * A nested conditional statement is an if or else if statement inside another else if statement.
+ */
