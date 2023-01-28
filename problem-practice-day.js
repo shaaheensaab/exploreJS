2f1283d3c65f6a40c32da055a439d50b501f3d1c
soloman <shaaheensaab@gmail.com>
2023-01-29 01:19:29 +0600

    problem-practice started

---
+++ problem-practice-day.js
@@ -0,0 +1,17 @@
+// :::::::::::PROBLEMS-PRACTICE:::::::::: \\
+/**
+1. What is javaScript? 
+    - javaScript is a programming language that is primarily used for creating interactive and dynamic web pages.
+
+2. How does javaScript works?
+    - javaScript is a client-side scripting language. Which means that is runs on the user's browser rather than on a server. It allows web developers to add interactivity, dynamic content, and other advanced feature to websites.
+
+3. What is Variable?
+    - A javaScript variable is a name of storage/ location.
+
+4. How does the variable declare?
+    - (var myName = "XYZ";) These 5 things inside the bracket need to remember during declaring a variable.
+
+5. How does update/ change value of a variable?
+    - 
+ */
